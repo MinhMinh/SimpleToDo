df4ece3d6dc85bdc13c2e1f8f3073196e081dc8c
MinhMinh <minhdanghuu@gmail.com>
2015-04-14 09:16:58 -0700

    add: TodoDatabaseHelper

--- app/src/main/java/com/nutanix/minhdang/simpletodo/MainActivity.java
+++ app/src/main/java/com/nutanix/minhdang/simpletodo/MainActivity.java
@@ -1,52 +1,48 @@
 package com.nutanix.minhdang.simpletodo;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
+import java.util.List;
 
 public class MainActivity extends Activity {
 
-    ArrayList<String> items;
-    ArrayAdapter<String> itemsAdapter;
-    ListView lvItems;
-    private final int REQUEST_CODE = 20;
+    private final String TAG = "MainActivity";
+
+    TodoItemDatabase db;
+    List<TodoItem> items;
+    //TodoAdapter itemsAdapter;
+    //ListView lvItems;
+    //private final int REQUEST_CODE = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new TodoItemDatabase(this);
+        db.addTodoItem(new TodoItem("One", 1));
+        db.addTodoItem(new TodoItem("Two", 2));
+
+        items = db.getAllTodoItems();
+
+        for (TodoItem ti : items) {
+            String log = "Id: " + ti.getId() + " , Body: " + ti.getBody() +
+                    " , Priority: " + ti.getPriority();
+
+            Log.d("Name: ", log);
+        }
+
+        /*
         lvItems = (ListView) findViewById(R.id.lvItems);
         readItems();
-        itemsAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, items);
+        itemsAdapter = new TodoAdapter(this, items);
         lvItems.setAdapter(itemsAdapter);
         setupListViewListener();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
-            String item_name = data.getExtras().getString("item_name");
-            int pos = data.getExtras().getInt("position", 0);
-            items.remove(pos);
-            items.add(pos, item_name);
-            itemsAdapter.notifyDataSetChanged();
-            writeItems();
-        }
+        */
     }
 
     @Override
@@ -71,12 +67,25 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            String item_name = data.getExtras().getString("item_name");
+            int pos = data.getExtras().getInt("position", 0);
+            items.remove(pos);
+            items.add(pos, new TodoItem(item_name, 1));
+            itemsAdapter.notifyDataSetChanged();
+            writeItems();
+        }
+    }
+
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
+        itemsAdapter.add(new TodoItem(itemText, 1));
         etNewItem.setText("");
-        writeItems();
+        db.addTodoItem(new TodoItem(itemText, 1));
     }
 
     private void setupListViewListener() {
@@ -85,9 +94,9 @@ public class MainActivity extends Activity {
                     @Override
                     public boolean onItemLongClick(AdapterView<?> adapterView,
                                                    View view, int pos, long id) {
+                        db.deleteTodoItem(items.get(pos));
                         items.remove(pos);
                         itemsAdapter.notifyDataSetChanged();
-                        writeItems();
                         return true;
                     }
                 }
@@ -99,7 +108,7 @@ public class MainActivity extends Activity {
                     public void onItemClick(AdapterView<?> adapterView,
                                             View view, int pos, long id) {
                         Intent i = new Intent(MainActivity.this, EditItemActivity.class);
-                        i.putExtra("item_name", items.get(pos));
+                        i.putExtra("item_name", items.get(pos).getBody());
                         i.putExtra("position", pos);
                         startActivityForResult(i, REQUEST_CODE);
                     }
@@ -108,13 +117,8 @@ public class MainActivity extends Activity {
     }
 
     private void readItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File(filesDir, "todo.txt");
-        try {
-            items = new ArrayList<String>(FileUtils.readLines(todoFile));
-        } catch (IOException e){
-            items = new ArrayList<String>();
-        }
+        Log.d(TAG, "readItems");
+        items = new ArrayList<TodoItem>(db.getAllTodoItems());
     }
 
     private void writeItems() {
@@ -126,4 +130,5 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         }
     }
+    */
 }
